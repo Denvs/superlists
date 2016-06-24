c100298a34825fd771fc1d85d4e13edc2c1c865d
denv <denv@example.com>
2016-06-24 13:54:02 +0300

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -61,6 +61,12 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc#
         self.browser.quit()
+
+        # My own lines to prevent WinError 10054 "Удаленный хост
+        # принудительно разорвал существующее подключение"
+        #import time
+        #time.sleep(10)
+
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's list
@@ -85,5 +91,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+
         # Satisfied, they both go back to sleep
-        
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=7
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=7
+        )
--- lists/migrations/0004_item_list.py
+++ lists/migrations/0004_item_list.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='item',
             name='list',
-            field=models.ForeignKey(to='lists.List', default=None),
+            field=models.ForeignKey(to='lists.List', default=None, null=True, blank=True),
         ),
     ]
